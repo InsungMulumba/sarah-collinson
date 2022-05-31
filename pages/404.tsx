0aad14946c9bbac81632ace9e195ea737e4eae59
Insung <insungmul@live.co.uk>
2022-05-31 10:29:55 +0100

    fix build 3

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,34 @@
+import { GetStaticProps } from "next";
 import React, { FC } from "react";
-
+import MainLayout from "../layouts/mainLayout";
+import PageWithLayoutType from "../types/pageWithLayout";
 
 const FourOhFour: FC = () => {
-    return (
-      <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', width: "100%", height: "calc(60vh)" }}>
-        <h1>404 - Page Not Found</h1>
-      </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "calc(60vh)",
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+(FourOhFour as PageWithLayoutType).layout = MainLayout;
+
+export default FourOhFour;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = "test";
+
+  return {
+    props: {
+      res,
+    },
   };
-  
-
-
-  export default FourOhFour;
+};
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
 import React, { FC } from "react";
+import MainLayout from "../layouts/mainLayout";
+import PageWithLayoutType from "../types/pageWithLayout";
 
 const FiveHundred: FC = () => {
   return <h1>500 - Page Not Found</h1>;
 };
 
+(FiveHundred as PageWithLayoutType).layout = MainLayout;
+
 export default FiveHundred;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = "test";
+
+  return {
+    props: {
+      res,
+    },
+  };
+};
